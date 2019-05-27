278c9b906cc406af45a1d763d01c964940e176f5
Niv Suresh <nivsuresh06@gmail.com>
2019-05-27 10:57:30 +0100

    Change into a functional component

--- src/components/Articles/ArticleItem.js
+++ src/components/Articles/ArticleItem.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import '../../sass/_articleContainer.scss'
+import React, {Component} from 'react'
+import '../../sass/_articleItem.scss'
 
-const ArticleItem = ({item}) => {
-    console.log(item)
-    const {title, sector, product, platform, image, desc, url, clientname } = item
-    return (
-        <div className='article-item' style={{backgroundImage: `url(${image})`}}>
-            <h2>{clientname}</h2>
-            <h1>{title}</h1>
-            <p>{product}</p>
-            <p>{platform}</p>
-            <p>{url}</p>
-        </div>
-    )
+class ArticleItem extends Component {
+    render() {
+        const {title, sector, product, platform, image, desc, url, clientname} = this.props.item
+
+        function platformLogic(plat) {
+            let x = null
+
+            return plat === '1' ? x = 'FT.com' : x = 'How To Spend It'
+        }
+        function sectorLogic(sect) {
+
+        }
+
+        function ItemOnClick(a, b) {
+            console.log(a)
+            console.log(b)
+        }
+        return(
+            <div className='article-item' style={{backgroundImage: `url(${image})`}} onClick={ItemOnClick.bind(this, url)}>
+                <h2>{clientname}</h2>
+                <h1>{title}</h1>
+                <p>{product}</p>
+                <p>{platformLogic(platform)}</p>
+                <p>{url}</p>
+            </div>
+        )
+    }
 }
 export default ArticleItem
