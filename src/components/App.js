1480939057aa9f7c1ab7571c92ae9b4d4e40403a
Niv Suresh <nivsuresh06@gmail.com>
2019-05-28 22:04:40 +0100

    Adding Article page to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
 import React, {Component} from "react"
 import {Consumer} from "../context"
 
+import Header from './Includes/Header'
+import Footer from './Includes/Footer'
+
+import HeroBanner from './Includes/HeroBanner'
+
 import ArticleContainer from './Articles/ArticleContainer'
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <React.Fragment>
+                <Header/>
+                <HeroBanner
+                    title="Welcome to the Financial Times' branded content hub"
+                    subHeading="Scroll down to see all of the client-voiced content marketing projects published by the FT.
+                                If you'd like any more information on any one project, you can get in touch with us here"
+                />
                 <Consumer>
                     {value => {
-                        return(
+                        return (
                             <ArticleContainer articles={value.article_list}/>
                         )
                     }}
                 </Consumer>
-            </div>
+                <Footer/>
+            </React.Fragment>
 
         )
     }
--- src/components/Articles/ArticleContainer.js
+++ src/components/Articles/ArticleContainer.js
@@ -6,12 +6,14 @@ import '../../sass/_articleContainer.scss'
 class ArticleContainer extends Component {
     render() {
         return (
-            <div className='article-container'>
-                {this.props.articles && this.props.articles.map((article, i) => {
-                    return(
-                        <ArticleItem key={i} item={article}/>
-                    )
-                })}
+            <div className="wrap">
+                <div className='article-container'>
+                    {this.props.articles && this.props.articles.map((article, i) => {
+                        return (
+                            <ArticleItem key={i} item={article}/>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
