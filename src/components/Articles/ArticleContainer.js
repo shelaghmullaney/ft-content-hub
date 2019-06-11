4ff102152f87129e5022cd3fa56b96efcebe836c
Niv Suresh <nivsuresh06@gmail.com>
2019-06-11 17:11:49 +0100

    Cleaned up ArticleItem render

--- src/components/Articles/ArticleContainer.js
+++ src/components/Articles/ArticleContainer.js
@@ -1,20 +1,66 @@
 import React, {Component} from 'react'
+import {Consumer} from "../../context"
 
 import ArticleItem from './ArticleItem'
 import '../../sass/_articleContainer.scss'
 
 class ArticleContainer extends Component {
     render() {
+
+        const SearchHeader = ({sector, searchVal, clear}) => {
+            let returnVal = ''
+            if (sector && !searchVal) {
+                returnVal = `Searching for : ${sector}`
+            }
+            if (searchVal && !sector) {
+                returnVal = `Searching for : ${searchVal}`
+            }
+            if (searchVal && sector) {
+                returnVal = `Searching for : ${searchVal} & ${sector}`
+            }
+
+            if (searchVal || sector) {
+                return (
+                    <div>
+                        {returnVal}
+                        <button onClick={clear.bind(this)}> Clear</button>
+                    </div>
+                )
+            } else {
+                return null
+            }
+        }
+
+        const ArticleItemWrapper = ({articleList, searchFilter, sectorFilter, articleTagFilter}) => {
+            return (
+                articleList.filter(searchFilter).filter(sectorFilter).map((article, i) => {
+                    return (
+                        <ArticleItem tagFilter={articleTagFilter} key={i} item={article}/>
+                    )
+                })
+            )
+        }
+
+
         return (
-            <div className="wrap">
-                <div className='article-container'>
-                    {this.props.articles && this.props.articles.map((article, i) => {
-                        return (
-                            <ArticleItem key={i} item={article}/>
-                        )
-                    })}
-                </div>
-            </div>
+            <Consumer>
+                {value => {
+                    const {sector, searchVal, clearSearchValue, articleList, handleArticleTagFilter} = value
+                    const {search, sectorSearch} = this.props
+                    return (
+                        <div className="wrap">
+                            <SearchHeader sector={sector} searchVal={searchVal} clear={clearSearchValue}/>
+                            <div className='article-container' id='articleContainer'>
+                                <ArticleItemWrapper articleList={articleList}
+                                                    searchFilter={search}
+                                                    sectorFilter={sectorSearch}
+                                                    articleTagFilter={handleArticleTagFilter}
+                                />
+                            </div>
+                        </div>
+                    )
+                }}
+            </Consumer>
         )
     }
 }
