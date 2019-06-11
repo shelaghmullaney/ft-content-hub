b26b34e99c905222f83cac99308048fc5a34a344
Niv Suresh <nivsuresh06@gmail.com>
2019-06-11 17:11:29 +0100

    ArticleItem updated + scss

--- src/components/Articles/ArticleItem.js
+++ src/components/Articles/ArticleItem.js
@@ -1,32 +1,49 @@
-import React, {Component} from 'react'
-import '../../sass/_articleItem.scss'
+import React, { Component } from "react"
+import "../../sass/_articleItem.scss"
+
 
 class ArticleItem extends Component {
-    render() {
-        const {title, sector, product, platform, image, desc, url, clientname} = this.props.item
+	render() {
+		const { title, sector, product, platform, image, desc, url, clientname } = this.props.item
 
-        function platformLogic(plat) {
-            let x = null
+		const SectorContainer = ({tag}) => {
+			let sectionArr = sector.split(',')
+			return sectionArr.map((sec, i) => {
+				return (
+					<li key={i} data-value={sec} onClick={tag.bind(this)}>{sec}</li>
+				)
+			})
+		}
 
-            return plat === '1' ? x = 'FT.com' : x = 'How To Spend It'
-        }
+		function platformLogic(plat) {
+			let x = null
 
-        function sectorLogic(sect) {
+			return plat === "1" ? (x = "FT.com") : (x = "How To Spend It")
+		}
 
-        }
+		function sectorLogic(sect) {}
 
-        function ItemOnClick(url) {
-            window.open(url);
-        }
-        return(
-            <div className='article-item' style={{backgroundImage: `url(${image})`}} onClick={ItemOnClick.bind(this, url)}>
-                <h2>{clientname}</h2>
-                <h1>{title}</h1>
-                <p>{product}</p>
-                <p>{platformLogic(platform)}</p>
-                <p>{url}</p>
-            </div>
-        )
-    }
+		function ItemOnClick(url) {
+		}
+
+		return (
+				<div
+					className='article-item'
+					style={{ backgroundImage: `url(${image})` }}
+					onClick={ItemOnClick.bind(this, url)}
+				>
+					<div className='article-item__content'>
+						<div className='article-item__client-name'> {clientname} </div>
+						<div className='article-item__title'>{title}</div>
+
+						<p>{product}</p>
+						<p>{platformLogic(platform)}</p>
+
+						<SectorContainer tag={this.props.tagFilter}/>
+					</div>
+				</div>
+
+		)
+	}
 }
 export default ArticleItem
